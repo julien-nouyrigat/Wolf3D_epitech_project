08c86cd3b05cac9d785cbe6eb84c1caa9d7ea205
Inès RABEAU <ines.rabeau@epitech.eu>
2026-04-16 23:16:00 +0200

    fix: removed unused variable

--- src/2d_raycasting/draw_2d_player.c
+++ src/2d_raycasting/draw_2d_player.c
@@ -9,7 +9,6 @@
 
 void draw_2d_player(window_t *wolf_win, player_t *player)
 {
-    sfVector2f p_vertex = (sfVector2f){player->pos_x, player->pos_y};
     sfVector2f position = (sfVector2f){player->pos_x, player->pos_y};
 
     sfRectangleShape_setPosition(player->hitbox, position);

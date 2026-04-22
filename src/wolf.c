bc953b1d5ca2b2c8d54f8291680ae45422582fcc
Antoine <antoine.lesueur@epitech.eu>
2026-04-22 08:20:29 +0200

    feat: loop music menu

--- src/wolf.c
+++ src/wolf.c
@@ -60,6 +60,7 @@ static int game_loop(window_t *wolf_win, player_t *player, map_t *map)
     sfVector2i mouse_pos;
 
     sfMusic_play(wolf_win->menu.music);
+    sfMusic_setLoop(wolf_win->menu.music, sfTrue);
     while (sfRenderWindow_isOpen(wolf_win->window)) {
         mouse_pos = sfMouse_getPositionRenderWindow(wolf_win->window);
         while (sfRenderWindow_pollEvent(wolf_win->window, &(wolf_win->event)))

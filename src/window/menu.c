009e2af315a738d16104c9340754649d2128fac4
Antoine <antoine.lesueur@epitech.eu>
2026-04-22 16:21:31 +0200

    fix: refacto struct menu

--- src/window/menu.c
+++ src/window/menu.c
@@ -9,11 +9,8 @@
 
 static void display_tab(window_t *win)
 {
-    sfRenderWindow_drawText(win->window, win->menu.host, NULL);
-    sfRenderWindow_drawText(win->window, win->menu.join, NULL);
-    sfRenderWindow_drawText(win->window, win->menu.single, NULL);
-    sfRenderWindow_drawText(win->window, win->menu.settings, NULL);
-    sfRenderWindow_drawText(win->window, win->menu.quit, NULL);
+    for (int i = 0; i < NB_TAB_MENU; i++)
+        sfRenderWindow_drawText(win->window, win->menu.tab[i].text, NULL);
 }
 
 static void display_title(window_t *win)
@@ -33,55 +30,54 @@ static void display_title(window_t *win)
 
 static void change_color_action(window_t *win, sfVector2i *mp, sfColor *grey)
 {
-    if (sfFloatRect_contains(&(win->menu.settings_bound), mp->x, mp->y)) {
+    if (sfFloatRect_contains(&(win->menu.tab[3].bound), mp->x, mp->y)) {
         sfSprite_setPosition(win->menu.s_rect_back, (sfVector2f){100, 775});
         sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.12, 1.2});
-        sfText_setColor(win->menu.settings, sfWhite);
+        sfText_setColor(win->menu.tab[3].text, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
     } else {
-        sfText_setColor(win->menu.settings, *grey);
+        sfText_setColor(win->menu.tab[3].text, *grey);
     }
-    if (sfFloatRect_contains(&(win->menu.quit_bound), mp->x, mp->y)) {
+    if (sfFloatRect_contains(&(win->menu.tab[4].bound), mp->x, mp->y)) {
         sfSprite_setPosition(win->menu.s_rect_back, (sfVector2f){100, 895});
         sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.24, 1.2});
-        sfText_setColor(win->menu.quit, sfWhite);
+        sfText_setColor(win->menu.tab[4].text, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
     } else {
-        sfText_setColor(win->menu.quit, *grey);
+        sfText_setColor(win->menu.tab[4].text, *grey);
     }
 }
 
 static void change_color_solo(window_t *win, sfVector2i *mp, sfColor *grey)
 {
-    if (sfFloatRect_contains(&(win->menu.single_bound), mp->x, mp->y)) {
+    if (sfFloatRect_contains(&(win->menu.tab[2].bound), mp->x, mp->y)) {
         sfSprite_setPosition(win->menu.s_rect_back, (sfVector2f){100, 655});
         sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.62, 1.2});
-        sfText_setColor(win->menu.single, sfWhite);
+        sfText_setColor(win->menu.tab[2].text, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
     } else {
-        sfText_setColor(win->menu.single, *grey);
+        sfText_setColor(win->menu.tab[2].text, *grey);
     }
-    if (!sfFloatRect_contains(&(win->menu.host_bound), mp->x, mp->y) &&
-        !sfFloatRect_contains(&(win->menu.join_bound), mp->x, mp->y) &&
-        !sfFloatRect_contains(&(win->menu.single_bound), mp->x, mp->y) &&
-        !sfFloatRect_contains(&(win->menu.settings_bound), mp->x, mp->y) &&
-        !sfFloatRect_contains(&(win->menu.quit_bound), mp->x, mp->y))
-        sfSprite_setPosition(win->menu.s_rect_back, (sfVector2f){-1000, -1000});
 }
 
 static void change_color_multi(window_t *win, sfVector2i *mp)
 {
     sfColor grey = sfColor_fromRGB(143, 143, 143);
 
-    if (sfFloatRect_contains(&(win->menu.host_bound), mp->x, mp->y)) {
+    if (sfFloatRect_contains(&(win->menu.tab[0].bound), mp->x, mp->y)) {
         sfSprite_setPosition(win->menu.s_rect_back, (sfVector2f){100, 415});
         sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.32, 1.2});
-        sfText_setColor(win->menu.host, sfWhite);
+        sfText_setColor(win->menu.tab[0].text, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
     } else
-        sfText_setColor(win->menu.host, grey);
-    if (sfFloatRect_contains(&(win->menu.join_bound), mp->x, mp->y)) {
+        sfText_setColor(win->menu.tab[0].text, grey);
+    if (sfFloatRect_contains(&(win->menu.tab[1].bound), mp->x, mp->y)) {
         sfSprite_setPosition(win->menu.s_rect_back, (sfVector2f){100, 535});
         sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.27, 1.2});
-        sfText_setColor(win->menu.join, sfWhite);
+        sfText_setColor(win->menu.tab[1].text, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
     } else
-        sfText_setColor(win->menu.join, grey);
+        sfText_setColor(win->menu.tab[1].text, grey);
     change_color_solo(win, mp, &grey);
     change_color_action(win, mp, &grey);
 }
@@ -91,13 +87,6 @@ static void display_rect(window_t *win)
     sfVector2i mp = sfMouse_getPositionRenderWindow(win->window);
 
     change_color_multi(win, &mp);
-    sfRenderWindow_drawRectangleShape(win->window, win->menu.host_rect, NULL);
-    sfRenderWindow_drawRectangleShape(win->window, win->menu.join_rect, NULL);
-    sfRenderWindow_drawRectangleShape(win->window, win->menu.single_rect, NULL);
-    sfRenderWindow_drawRectangleShape(win->window, win->menu.settings_rect,
-        NULL);
-    sfRenderWindow_drawRectangleShape(win->window, win->menu.quit_rect, NULL);
-    sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
 }
 
 void display_menu(window_t *win)

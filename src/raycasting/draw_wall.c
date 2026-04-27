0ef9713df0e59184a15042709d9f78595755ce14
Inès RABEAU <ines.rabeau@epitech.eu>
2026-04-27 23:42:13 +0200

    feat: wall textures

--- src/raycasting/draw_wall.c
+++ src/raycasting/draw_wall.c
@@ -10,40 +10,6 @@
 #include "textures.h"
 #include "wolf.h"
 
-// static float find_wall_x(ray_t *ray)
-// {
-//     float wall_x = ray->orientation == VERTICAL ?
-//         ray->screen_x + ray->real_dist * ray->direction.x :
-//         ray->screen_x + ray->real_dist * ray->direction.y;
-
-//     wall_x -= floor(wall_x);
-//     return wall_x;
-// }
-
-// void draw_wall(ray_t *ray, window_t *win, map_t *map, player_t *player)
-// {
-//     int wall_height = (int)(win->size.y / ray->real_dist);
-//     int draw_start = (win->size.y / 2) - (wall_height / 2);
-//     int draw_end = (win->size.y / 2) + (wall_height / 2);
-//     float wall_x = find_wall_x(ray);
-//     sfVector2u text_size = sfTexture_getSize
-//         (win->textures[(map->type * TEXT_TYPES + WALL) - TEXT_TYPES]);
-//     float text_x = wall_x * text_size.x;
-//     sfVertexArray *wall_line = sfVertexArray_create();
-//     sfVertex bottom = {.position = {ray->screen_x, draw_start},
-//         .texCoords = {text_x, 0.0}};
-//     sfVertex top = {.position = {ray->screen_x, draw_end},
-//         .texCoords = (sfVector2f){text_size.x, text_size.y}};
-
-//     sfVertexArray_append(wall_line, bottom);
-//     sfVertexArray_append(wall_line, top);
-//     sfVertexArray_setPrimitiveType(wall_line, sfLines);
-//     sfRenderWindow_drawVertexArray(win->window, wall_line, &wall);
-//     sfVertexArray_destroy(wall_line);
-// }
-
-
-
 static float find_wall_x(ray_t *ray, player_t *player)
 {
     float wall_x = ray->orientation == VERTICAL ?
@@ -54,25 +20,59 @@ static float find_wall_x(ray_t *ray, player_t *player)
     return wall_x;
 }
 
+static sfVertex create_vertex(float pos_x, float pos_y, float text_x,
+    float text_y)
+{
+    sfVertex vertex = {.position = (sfVector2f){pos_x, pos_y},
+        .color = sfWhite, .texCoords = (sfVector2f){text_x, text_y}};
+
+    return vertex;
+}
+
+static sfRenderStates get_wall_state(window_t *win, map_t *map)
+{
+    sfRenderStates wall_state = {.texture =
+            win->textures[(map->type * TEXT_TYPES + WALL) - TEXT_TYPES - 1],
+        .blendMode = sfBlendAlpha, .transform = sfTransform_Identity,
+        .shader = NULL};
+
+    return wall_state;
+}
+
+static sfVertexArray *create_wall_array(sfVertex *top_left,
+    sfVertex *top_right, sfVertex *bottom_right,
+    sfVertex *bottom_left)
+{
+    sfVertexArray *wall_line = sfVertexArray_create();
+
+    sfVertexArray_setPrimitiveType(wall_line, sfQuads);
+    sfVertexArray_append(wall_line, *top_left);
+    sfVertexArray_append(wall_line, *top_right);
+    sfVertexArray_append(wall_line, *bottom_right);
+    sfVertexArray_append(wall_line, *bottom_left);
+    return wall_line;
+}
+
 void draw_wall(ray_t *ray, window_t *win, map_t *map, player_t *player)
 {
     int wall_height = (int)(win->size.y / ray->real_dist);
+    int wall_bottom = (win->size.y / 2) - (wall_height / 2);
+    int wall_top = (win->size.y / 2) + (wall_height / 2);
     float wall_x = find_wall_x(ray, player);
-    int draw_start = (win->size.y / 2) - (wall_height / 2);
-    int draw_end = (win->size.y / 2) + (wall_height / 2);
-    sfVector2u text_size = sfTexture_getSize
-        (win->textures[(map->type * TEXT_TYPES + WALL) - TEXT_TYPES]);
-    sfVector2i text_coord = (sfVector2i){0, 0};
-    float step = 1.0 * text_size.y / wall_height;
-    float text_pos = (draw_start - win->size.y / 2 + wall_height / 2) * step;
+    sfVector2u size = sfTexture_getSize
+        (win->textures[(map->type * TEXT_TYPES + WALL) - TEXT_TYPES - 1]);
+    float text_x = wall_x * size.x;
+    sfVertex top_left = create_vertex(ray->screen_x, wall_top, text_x, size.y);
+    sfVertex top_right =
+        create_vertex(ray->screen_x + 1.0, wall_top, text_x + 1.0, size.y);
+    sfVertex bottom_right = create_vertex(ray->screen_x + 1.0, wall_bottom,
+        text_x + 1.0, 0.f);
+    sfVertex bottom_left =
+        create_vertex(ray->screen_x, wall_bottom, text_x, 0.f);
+    sfRenderStates wall_state = get_wall_state(win, map);
+    sfVertexArray *wall_line = create_wall_array(&top_left, &top_right,
+        &bottom_left, &bottom_right);
 
-    text_coord.x = (int)(wall_x * text_size.x);
-    if ((ray->orientation == VERTICAL && ray->direction.x > 0) ||
-        (ray->orientation == HORIZONTAL && ray->direction.y < 0))
-        text_coord.x = text_size.x - text_coord.x - 1;
-    for (int i = draw_start; i < draw_end; i++) {
-        text_coord.y = (int)text_pos & (text_size.y - 1);
-        text_pos += step;
-        ray->color = sfRed;
-    }
+    sfRenderWindow_drawVertexArray(win->window, wall_line, &wall_state);
+    sfVertexArray_destroy(wall_line);
 }

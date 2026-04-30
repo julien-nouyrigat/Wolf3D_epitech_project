215526be852c37d3d87eab441ac617bf50936551
Antoine <antoine.lesueur@epitech.eu>
2026-04-30 10:44:10 +0200

    feat: init join

---
+++ src/window/init/init_join.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** Wolf3D
+** File description:
+** init_join
+*/
+
+#include "wolf.h"
+
+static int init_text(window_t *win)
+{
+}
+
+int init_join(window_t *win)
+{
+
+}

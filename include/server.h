7cfc9ab0487bd7932389982af2efccb5f94c2dd3
Antoine <antoine.lesueur@epitech.eu>
2026-05-06 11:40:42 +0200

    feat: server.h

--- include/server.h
+++ include/server.h
@@ -14,11 +14,15 @@
     #define LOCAL "127.0.0.1"
     #define EPOLL_EVENT 64
     #define USERNAME_SIZE 24
+    #define EXIT_PROG -3
 
     #include <sys/socket.h>
     #include <arpa/inet.h>
     #include <sys/epoll.h>
+    #include <stdbool.h>
     #include <stdio.h>
+
+    #include "wolf.h"
 
 typedef struct {
     uint8_t player_fd;
@@ -28,10 +32,29 @@ typedef struct {
 } state_t;
 
 typedef struct {
+    uint8_t client_id;
+    struct sockaddr_in sa_in;
+    int fd_tcp;
+    bool is_host;
+} client_t;
+
+typedef struct {
+    char ip[BUFSIZ];
+    client_t clients[CLIENT_MAX];
     int sock_tcp;
     int sock_udp;
     int fd_epoll;
     int nb_clients;
+    bool is_launch;
+    map_t *map;
 } servert_t;
+
+int init_epoll(servert_t *serv);
+int init_tcp(servert_t *serv);
+void get_ip(servert_t *serv);
+int server_loop(servert_t *serv);
+int check_request(servert_t *serv);
+int manage_client(servert_t *serv, int fd);
+int accept_client(servert_t *serv);
 
 #endif

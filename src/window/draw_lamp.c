d36af76427dc6200b31e891030de232b72d9217d
Julien Nouyrigat <julien.nouyrigat@gmail.com>
2026-05-18 11:55:13 +0200

    feat: change lamp dl

--- src/window/draw_lamp.c
+++ src/window/draw_lamp.c
@@ -9,11 +9,11 @@
 
 void display_lamp(window_t *win)
 {
-    sfVector2f pos_lamp = {150, 676};
-    sfVector2f pos_dark = {-300, -150};
+    sfVector2f pos_lamp = {90, 676};
+    sfVector2f pos_dark = {-240, -100};
 
-    sfRenderWindow_drawSprite(win->window, win->game.lamp, NULL);
-    sfSprite_setPosition(win->game.lamp, pos_lamp);
     sfRenderWindow_drawSprite(win->window, win->game.dark, NULL);
     sfSprite_setPosition(win->game.dark, pos_dark);
+    sfRenderWindow_drawSprite(win->window, win->game.lamp, NULL);
+    sfSprite_setPosition(win->game.lamp, pos_lamp);
 }

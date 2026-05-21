72d08a81063e42755dcfda838a09a8b3d716baa2
Julien Nouyrigat <julien.nouyrigat@gmail.com>
2026-05-21 20:54:59 +0200

    feat: refacto player movement

--- src/player_movements/movements.c
+++ src/player_movements/movements.c
@@ -17,26 +17,35 @@ static int get_verif(float pos, float dir, player_t *player)
     return res;
 }
 
-static void move(player_t *player, map_t *map, sfVector2f *movement_dir)
+static void change_pos(map_t *map, player_t *player,
+    sfVector2f *movement_dir, sfVector2f *verif)
 {
-    int x_verif = get_verif(player->position.x, movement_dir->x, player);
-    int y_verif = get_verif(player->position.y, movement_dir->y, player);
-
-    if (player->is_in_inv)
-        return;
-    player->bobing += BOBING_COEF * (player->sprint + 1);
-    if (map->int_map[(int)(player->position.y / TILE_SIZE)][x_verif] != WALL) {
+    if (map->int_map[(int)(player->position.y / TILE_SIZE)]
+        [(int)verif->x] != WALL) {
         player->position.x += movement_dir->x *
             (player->mvt_speed + SPRINT * player->sprint);
         player->pos_f.x = player->position.x / TILE_SIZE;
         player->is_moving = true;
     }
-    if (map->int_map[y_verif][(int)(player->position.x / TILE_SIZE)] != WALL) {
+    if (map->int_map[(int)verif->y]
+        [(int)(player->position.x / TILE_SIZE)] != WALL) {
         player->position.y += movement_dir->y *
             (player->mvt_speed + SPRINT * player->sprint);
         player->pos_f.y = player->position.y / TILE_SIZE;
         player->is_moving = true;
     }
+}
+
+static void move(player_t *player, map_t *map, sfVector2f *movement_dir)
+{
+    sfVector2f verif = {0, 0};
+
+    verif.x = get_verif(player->position.x, movement_dir->x, player);
+    verif.y = get_verif(player->position.y, movement_dir->y, player);
+    if (player->is_in_inv)
+        return;
+    player->bobing += BOBING_COEF * (player->sprint + 1);
+    change_pos(map, player, movement_dir, &verif);
 }
 
 void move_forward(player_t *player, map_t *map)

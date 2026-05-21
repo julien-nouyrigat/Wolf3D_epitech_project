52b78c75d354bbd6c39b86beeef81dc9c2139c91
Antoine <antoine.lesueur@epitech.eu>
2026-05-21 15:27:05 +0200

    fix :coding style

--- src/initialization/init_window.c
+++ src/initialization/init_window.c
@@ -8,6 +8,15 @@
 #include "network.h"
 #include "wolf.h"
 
+static void init_assets(window_t *wolf_win)
+{
+    init_brackground_menu(wolf_win);
+    init_menu(wolf_win);
+    init_lamp(wolf_win);
+    init_client(wolf_win);
+    init_param(wolf_win);
+}
+
 int init_window(window_t *wolf_win)
 {
     sfVideoMode mode = {WIN_WIDTH, WIN_HEIGHT, WIN_BPP};
@@ -19,14 +28,11 @@ int init_window(window_t *wolf_win)
         return EXIT_FAILURE;
     wolf_win->size = sfRenderWindow_getSize(wolf_win->window);
     wolf_win->bg_color = sfBlack;
+    wolf_win->is_clickable = true;
     wolf_win->clock.clock = sfClock_create();
     wolf_win->clock.elapsed_time_bg = 0.0f;
     wolf_win->font = sfFont_createFromFile("./assets/fonts/teko.ttf");
-    init_brackground_menu(wolf_win);
-    init_menu(wolf_win);
-    init_lamp(wolf_win);
-    init_client(wolf_win);
-    init_param(wolf_win);
+    init_assets(wolf_win);
     if (create_textures(wolf_win) == EXIT_FAILURE)
         return EXIT_FAILURE;
     return EXIT_SUCCESS;

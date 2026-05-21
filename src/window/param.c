2023d34642433354436cc5dfe31bfd265eab484a
Antoine <antoine.lesueur@epitech.eu>
2026-05-21 05:37:12 +0200

    feat: check tabs params

--- src/window/param.c
+++ src/window/param.c
@@ -7,8 +7,58 @@
 
 #include "wolf.h"
 
-static void check_tab(window_t *win, sfVector2i *pos_mouse)
+static void check_controls(window_t *win, sfVector2i *pos_mouse)
 {
+    sfColor grey = sfColor_fromRGB(143, 143, 143);
+
+    if (sfFloatRect_contains(&win->param.tab[2].boud, pos_mouse->x,
+            pos_mouse->y)) {
+        sfSprite_setPosition(win->menu.s_rect_back,
+            sfText_getPosition(win->param.tab[2].tab));
+        sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.32, 1.2});
+        sfText_setColor(win->param.tab[2].tab, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
+        if (win->event.type == sfEvtMouseButtonPressed) {
+            printf("67\n");
+        }
+    } else
+        sfText_setColor(win->param.tab[2].tab, grey);
+}
+
+static void check_audio(window_t *win, sfVector2i *pos_mouse)
+{
+    sfColor grey = sfColor_fromRGB(143, 143, 143);
+
+    if (sfFloatRect_contains(&win->param.tab[1].boud, pos_mouse->x,
+            pos_mouse->y)) {
+        sfSprite_setPosition(win->menu.s_rect_back,
+            sfText_getPosition(win->param.tab[1].tab));
+        sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.32, 1.2});
+        sfText_setColor(win->param.tab[1].tab, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
+        if (win->event.type == sfEvtMouseButtonPressed) {
+            printf("67\n");
+        }
+    } else
+        sfText_setColor(win->param.tab[1].tab, grey);
+}
+
+static void check_graphic(window_t *win, sfVector2i *pos_mouse)
+{
+    sfColor grey = sfColor_fromRGB(143, 143, 143);
+
+    if (sfFloatRect_contains(&win->param.tab[0].boud, pos_mouse->x,
+            pos_mouse->y)) {
+        sfSprite_setPosition(win->menu.s_rect_back,
+            sfText_getPosition(win->param.tab[0].tab));
+        sfSprite_setScale(win->menu.s_rect_back, (sfVector2f){1.32, 1.2});
+        sfText_setColor(win->param.tab[0].tab, sfWhite);
+        sfRenderWindow_drawSprite(win->window, win->menu.s_rect_back, NULL);
+        if (win->event.type == sfEvtMouseButtonPressed) {
+            printf("67\n");
+        }
+    } else
+        sfText_setColor(win->param.tab[0].tab, grey);
 }
 
 static void check_back(window_t *win, sfVector2i *pos_mouse)
@@ -40,5 +90,8 @@ void display_param(window_t *win)
     }
     sfRenderWindow_drawText(win->window, win->param.settings, NULL);
     check_back(win, &pos_mouse);
+    check_graphic(win, &pos_mouse);
+    check_audio(win, &pos_mouse);
+    check_controls(win, &pos_mouse);
     display_cursor(win);
 }

d8b806c11e1c2f8475a1df3b35281b69fbca9b4e
Antoine <antoine.lesueur@epitech.eu>
2026-05-28 13:26:18 +0200

    fix: coding style

--- src/enemies/add_to_entity_list.c
+++ src/enemies/add_to_entity_list.c
@@ -9,6 +9,48 @@
 #include "entity_display.h"
 #include "wolf.h"
 
+int add_monster_entity(enemy_t *mob, entities_t **head,
+    player_t *player, map_t *map)
+{
+    entities_t *new = NULL;
+
+    if (mob->monster->health <= 0)
+        return EXIT_SUCCESS;
+    new = calloc(sizeof(entities_t), 1);
+    if (new == NULL)
+        return EXIT_FAILURE;
+    new->position = mob->monster->position;
+    new->direction = mob->monster->direction;
+    new->text = map->level->mob_texts[mob->type];
+    new->p_dist = sqrt(
+        pow(player->pos_f.x - mob->monster->position.x / TILE_SIZE, 2) +
+        pow(player->pos_f.y - mob->monster->position.y / TILE_SIZE, 2));
+    new->next = *head;
+    *head = new;
+    return EXIT_SUCCESS;
+}
+
+int add_items_entity(loot_t *loot, entities_t **head,
+    player_t *player)
+{
+    entities_t *new = NULL;
+
+    if (loot->item->is_grab)
+        return EXIT_SUCCESS;
+    new = calloc(sizeof(entities_t), 1);
+    if (new == NULL)
+        return EXIT_FAILURE;
+    new->position = loot->item->position;
+    new->direction = loot->item->direction;
+    new->text = loot->item->textures;
+    new->p_dist = sqrt(
+        pow(player->pos_f.x - loot->item->position.x / TILE_SIZE, 2) +
+        pow(player->pos_f.y - loot->item->position.y / TILE_SIZE, 2));
+    new->next = *head;
+    *head = new;
+    return EXIT_SUCCESS;
+}
+
 void add_player_entity(entities_t **head, player_t *player,
     window_t *win, size_t i)
 {

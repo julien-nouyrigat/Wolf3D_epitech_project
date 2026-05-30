3c3a757bcbd6322174aa6670af32c2ecaf13b886
Inès RABEAU <ines.rabeau@epitech.eu>
2026-05-30 22:03:32 +0200

    feat: stamina reinitialization for each new level

--- src/level_management/next_level.c
+++ src/level_management/next_level.c
@@ -32,6 +32,7 @@ static int change_map(map_t *map, size_t lvl_id)
 static void reinit_player(player_t *player)
 {
     init_player_comp(&player);
+    player->stamina = player->max_stamina;
     sfClock_restart(player->p_clock);
 }
 

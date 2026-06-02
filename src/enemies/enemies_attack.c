565eec976be04f7a12498e613dc5131595440252
Julien Nouyrigat <julien.nouyrigat@gmail.com>
2026-06-02 13:37:46 +0200

    change enemies attack

--- src/enemies/enemies_attack.c
+++ src/enemies/enemies_attack.c
@@ -21,10 +21,14 @@ void take_damage(player_t *player, size_t damage)
 void enemy_attack(player_t *player, map_t *map)
 {
     enemy_t *enemies = map->level->enemies;
+    float dx = 0;
+    float dy = 0;
 
     for (; enemies; enemies = enemies->next){
-        if (enemies->monster->position.x - player->position.x < 10 &&
-            enemies->monster->position.y - player->position.y < 10){
+        dx = enemies->monster->position.x - player->position.x;
+        dy = enemies->monster->position.y - player->position.y;
+        if (dx > -60 && dx < 60 &&
+            dy > -60 && dy < 60){
             take_damage(player, 1);
         }
     }

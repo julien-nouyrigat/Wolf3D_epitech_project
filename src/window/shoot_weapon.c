5170376b095616ee4a554b4a0e38be2f60a241c1
Inès RABEAU <ines.rabeau@epitech.eu>
2026-06-04 04:01:39 +0200

    fix: working gun animation, still needs adjustments

--- src/window/shoot_weapon.c
+++ src/window/shoot_weapon.c
@@ -19,18 +19,22 @@ static void reset_state(player_t *player)
 void animate_hand(player_t *player)
 {
     sfTime anim = sfClock_getElapsedTime(player->in_hand->animation);
+    int col = 0;
+    int row = 0;
 
     if (!player->shoots)
         return;
-    if (anim.microseconds / SECOND >= 0.2) {
+    col = player->in_hand->frame % 3;
+    row = player->in_hand->frame / 3;
+    if (anim.microseconds / SECOND >= 0.025) {
         sfClock_restart(player->in_hand->animation);
-        player->in_hand->area.left += player->in_hand->stats->d_left;
+        player->in_hand->area.left =
+            weapons_data[player->in_hand->type].area.left + col *
+            weapons_data[player->in_hand->type].d_left;
+        player->in_hand->area.top =
+            weapons_data[player->in_hand->type].area.top + row *
+            weapons_data[player->in_hand->type].d_top;
         player->in_hand->frame += 1;
-        if (player->in_hand->frame > 0 && player->in_hand->frame % 3 == 2) {
-            player->in_hand->area.left =
-                weapons_data[player->in_hand->type].area.left;
-            player->in_hand->area.top += player->in_hand->stats->d_top;
-        }
         if (player->in_hand->frame > 8) {
             reset_state(player);
         }

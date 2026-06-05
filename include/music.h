b953c14b4f603ba057d5ffdb8cce9cf25b8875cc
Julien Nouyrigat <julien.nouyrigat@gmail.com>
2026-06-05 04:07:12 +0200

    feat: add som rooms and song

--- include/music.h
+++ include/music.h
@@ -7,7 +7,7 @@
 
 #ifndef MUSIC_H_
     #define MUSIC_H_
-    #define NB_MUSIC 2
+    #define NB_MUSIC 10
     #include <SFML/Audio.h>
 
 typedef void(*func_music_t)(sfMusic *music, float *t1, float t2);
@@ -30,6 +30,14 @@ void play_music(sfMusic *music, float *t1, float t2);
 static const info_music_t info_music[NB_MUSIC] = {
     {100, "assets/sounds/67_Cymbal.mp3", &play_music},
     {101, "assets/sounds/74_Scare_Sighting.mp3", &play_music},
+    {102, "assets/sounds/02_Lobby.mp3", &play_music},
+    {103, "assets/sounds/13_Arctic_Water.mp3", &play_music},
+    {104, "assets/sounds/17_Manor_Outside_Metal.mp3", &play_music},
+    {105, "assets/sounds/26_Enemy_Near_01.mp3", &play_music},
+    {106, "assets/sounds/27_Enemy_Near_02.mp3", &play_music},
+    {107, "assets/sounds/28_Enemy_Near_03.mp3", &play_music},
+    {111, "assets/sounds/72_Scare_Impact.mp3", &play_music},
+    {112, "assets/sounds/monster_footstep.wav", &play_music},
 };
 
 #endif /* MUSIC_H_ */
